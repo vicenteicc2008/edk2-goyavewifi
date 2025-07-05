8f9bd536343f57f45b1e390c1a0a80669551f02d
Vicente Cortés <vicenteicc2008@gmail.com>
2025-07-05 13:01:40 -0400

    refactor SprdClock.h (SprdClock protocol)

--- MSM8909Pkg/Include/Protocol/SprdClock.h
+++ MSM8909Pkg/Include/Protocol/SprdClock.h
@@ -1,23 +1,13 @@
 #ifndef __CLOCK_MANAGEMENT_PROTOCOL_H__
 #define __CLOCK_MANAGEMENT_PROTOCOL_H__
 
-#define SPRD_CLOCK_MANAGEMENT_PROTOCOL_GUID \
+#define SPRD_CLOCK_PROTOCOL_GUID \
     { 0xb2c7cf0b, 0xf42d, 0x42d2, { 0x84, 0x03, 0x86, 0x12, 0x28, 0xdb, 0xc7, 0x6e } }
 
-typedef struct _SPRD_CLOCK_MGMT_PROTOCOL SPRD_CLOCK_MGMT_PROTOCOL;
+typedef struct _SPRD_CLOCK_PROTOCOL_GUID SPRD_CLOCK_PROTOCOL_GUID;
 
-typedef BOOLEAN    (EFIAPI *clk_sdmmc_query_clk_not_reset_and_enabled_t)(UINT32 Index);
-typedef EFI_STATUS (EFIAPI *clk_sdmmc_get_params_t)(UINT32 *pOut, UINT16 *pDivisor, UINT32 Type);
-typedef EFI_STATUS (EFIAPI *clk_sdmmc_enable_t)(UINT32 Index, UINT32 Clock);
-typedef EFI_STATUS (EFIAPI *clk_sdmmc_disable_t)(UINT32 Index);
-typedef EFI_STATUS (EFIAPI *clk_sdmmc_sel_clk_src_t)(UINT32 *pOut, UINT32 Index, UINT32 Value);
-
-struct _SPRD_CLOCK_MGMT_PROTOCOL {
-	clk_sdmmc_query_clk_not_reset_and_enabled_t QuerySdMmcClkNotResetAndEnabled;
-    clk_sdmmc_get_params_t  QuerySdMmcClkParams;
-    clk_sdmmc_enable_t      EnableSdMmcClk;
-    clk_sdmmc_sel_clk_src_t SelectSdMmcClkSource;
-    clk_sdmmc_disable_t     DisableSdMmcClk;
+struct _SPRD_CLOCK_PROTOCOL_GUID {
+	
 };
 
 extern EFI_GUID gSprdClockProtocolGuid;
